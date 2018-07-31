0d914e5ac55725caef89a562e16bd5823eb9b875
weilewei <lokwei9@gmail.com>
2018-07-30 20:26:10 -0500

    adding physl performance data

---
+++ Python_vs_Phylanx/30AUG2018/NN_PHYLANX/RunNNPHX_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "This script is used to run nn_physl.physl example"
+
+iteration_array=(800)
+learning_rate=(0.1)
+thr=(1 2 4 6 8 10 12 14 16)
+
+for it in "${iteration_array[@]}"
+    do
+    for lr in "${learning_rate[@]}"
+        do
+	  for th in "${thr[@]}"
+		do
+           	 export OMP_NUM_THREADS=1
+            		time ~/src/repos/phylanx/build_Release/bin/physl /home/wwei/tests/SLNN/nn_physl.physl ${lr} ${it} --hpx:threads=${th} --hpx:bind=balanced --hpx:numa-sensitive --hpx:print-counter=/threads{locality#*/total}/idle-rate >> nnphx_${th}th_itrscs_${it}_${lr}
+           	 echo "done ${th}_${it}_${lr}"
+       		 done
+    	done
+   done
+
---
+++ Python_vs_Phylanx/30AUG2018/NN_PYTHON/RunNNPy_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "This script is used to run nn_python example"
+
+
+#iteration_array=(10000)
+#iteration_array=(1 100 1000 5000 10000)
+#row_stop_array=(100 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+#row_stop_array=(10000)
+thr=(1 2 4 6 8 10 12 14 16 18 20)
+
+for th in "${thr[@]}"
+    do
+	export OMP_NUM_THREADS=${th}   
+        python3 /home/wwei/tests/SLNN/nn_python.py >> nn_${th}th_itrscs_800_0.1
+	echo "done ${th}"	
+done
